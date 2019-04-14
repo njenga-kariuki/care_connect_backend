c4a5568969f1efae3718984bb6940d84c75981e2
njenga <njengak@me.com>
2019-04-14 07:43:00 -0700

    first commit with initial code

---
+++ app/controllers/api/v1/caregivers_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::CaregiversController < ApplicationController
+  def index
+      [1,2,3] #hard coded test array
+
+      # @notes = xyz
+      # render json: @notes
+    end
+
+
+end
